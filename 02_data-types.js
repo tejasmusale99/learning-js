5809068bed7ca35b6d62b6465b3599877dd47978
Tejas Musale <tejasmusale99@gmail.com>
2025-02-11 23:13:08 +0530

    learned about all the data types

---
+++ 02_data-types.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js aa a new version
+
+let name= "tejas"; //string
+let age= 24; //number
+let isLoggedIn = true; //boolean
+let city;  //undefined
+let state = null;
+// BigInt 
+//symbol
+
+//  to check type of data
+
+console.log(typeof name);
+
