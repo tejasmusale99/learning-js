6a443e5cddd357554a3aef3de32b1dd98f6728aa
Tejas Musale <tejasmusale99@gmail.com>
2025-02-15 22:23:28 +0530

    conversion of data types

---
+++ 03_conversionOperation.js
@@ -0,0 +1,43 @@
+// convert number 
+
+/*
+let score = undefined;
+
+console.log(typeof score) 
+
+let valueNumber = Number(score);
+
+console.log(valueNumber)
+*/
+
+/*
+"33" =>  33 
+"33abc" => Nan
+true => 1 false => 0
+*/
+
+/*
+let isLoggedIn = "TEjas"
+
+console.log(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+*/
+
+/* 
+
+1 => true 
+0 => false 
+"" => false
+"Tejas" => true
+*/
+
+let someNumber = 33;
+
+console.log(typeof someNumber)
+
+let isReallyConvertToString = String(someNumber)
+
+console.log(typeof isReallyConvertToString)
