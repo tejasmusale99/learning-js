c6936ebe9a28c9356b3d47776be3e3dc3502ef9f
Tejas Musale <tejasmusale99@gmail.com>
2025-02-21 23:26:20 +0530

    learn comparision tricky concetps

---
+++ 04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+
+// console.log(2 <= 1);
+
+// When using <, >, <=, >= (comparisons) → JavaScript converts null to 0.
+// When using == (loose equality) → JavaScript does NOT convert null to 0.
+
+/*
+console.log(null > 0) // 0 > 0 false
+console.log(null < 0) // 0 < 0 false 
+console.log(null == 0) // null == 0 false (null is not convert to zero)
+console.log(null >= 0) //0 == 0 true
+*/
+
+// Strict Check ===
+
+console.log("2" == 2)  //true  check value
+console.log("2" === 2) //false check data type and value tooo
+
