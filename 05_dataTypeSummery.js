1cdf544cf1154cc12b8a133ecd0e12890f61994c
Tejas Musale <tejasmusale99@gmail.com>
2025-02-23 19:02:58 +0530

    data type summery

---
+++ 05_dataTypeSummery.js
@@ -0,0 +1,20 @@
+// Primitive Data Type And Non-Primitive Data type
+
+// Primitive 
+
+// 7 types (String, Number, Boolean, undefined, Null, Symbol, BigiInt)   
+
+// Reference (Non- Primitive data Type)
+
+// Array, Objects, functions    
+// let checkCopy = 15;
+
+// let checkCopy1 = checkCopy;
+
+// checkCopy1 =25;
+
+// console.log(checkCopy);
+// console.log(checkCopy1);
+
+
+
