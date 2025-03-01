460553a168ce2af5a2133d66b1e5c0da7b0f9b66
Tejas Musale <tejasmusale99@gmail.com>
2025-03-01 22:50:54 +0530

    moved file

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,43 @@
+// Let Keyword 
+
+/*
+
+let accountName = "Tejas";     
+accountName="shubham"    // reassign variable
+let accountName = "bro"; //cannot redeclare
+
+{
+    let accountName = "akshay"  
+    console.log(accountName);   block scope we can only access the varible inside the block cannot acces vaible outside that block
+}
+
+console.table(accountName);
+*/
+
+// var keyword
+
+/*
+
+var  accountCity = "Pimpalner"  
+accountCity ="nashik"    //reasssign and redclared both possible 
+var accountCity = "jalgaon"
+
+function action(){
+    var check = "neel"; //cannot access variable outside the function 
+}
+
+console.log(check)
+action()
+
+*/
+
+// Const keyword 
+
+// we cannot reassign and declared the variable 
+
+
+
+
+
+
+
