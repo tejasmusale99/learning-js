7d8645c895ee338991c5df614fc25028e5b6a094
Tejas Musale <tejasmusale99@gmail.com>
2025-03-01 22:48:52 +0530

    learned about dates

---
+++ 08_date.js
@@ -0,0 +1,14 @@
+let todayDate = new Date()
+
+console.log(todayDate)
+
+let myDate = new Date()
+
+console.log(myDate.toDateString())
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toString())
+
