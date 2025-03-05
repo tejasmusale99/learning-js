ddb5c1c8c73e2153ed6c2a9d54288576857e952d
Tejas Musale <tejasmusale99@gmail.com>
2025-03-05 23:36:45 +0530

    destructuring in object

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,26 @@
+// destrcturing 
+
+const course = {
+    coursename: "js Hindi",
+    price: "9999",
+    courseInstructor: "HiteshSir"
+}
+
+const {coursename, price, courseInstructor} = course
+
+console.log(course)
+console.log(coursename)
+
+//JSON (JAVASCRIPT OBJECT NOTATION)
+
+// IT IS NOTHING BUT OBJECT 
+
+// How JSON Looks ? key and value usually in string
+
+// {
+//     "name": "tejas",
+//     "age": 24,
+//     "id": "YKA_1256"
+// }
+
+
