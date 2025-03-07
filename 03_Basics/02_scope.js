a1570786a341c980b944edd18db454345676c37a
Tejas Musale <tejasmusale99@gmail.com>
2025-03-07 21:41:13 +0530

    learned about closures in js

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -8,12 +8,43 @@
 // }
  
 // console.log(a);
-function blockCheck() {
-  var a = 10;
-  console.log(a);
-}
+// function blockCheck() {
+//   var a = 10;
+//   console.log(a);
+// }
 
-blockCheck();
+// blockCheck();
 
 // console.log(b);
+
+
+/*********** Closures *************/
+
+const parentScope = "global"
+
+function parent(){
+  console.log(parentScope)
+  const username = "tejas"
+
+  function child(){
+    const tutor = "hitesh sir"
+    console.log(username)
+  }
+
+  // console.log(tutor)
+  child()
+
+}
+
+// parent()
+
+
+/************** function expression ***************/
+
+
+const addTwo = function(num){
+  return num + 1;
+}
+
+console.log(addTwo(5))
 
