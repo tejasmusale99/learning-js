d27674884b9c43b3f2f52fa5854aa88ab2d628a6
Tejas Musale <tejasmusale99@gmail.com>
2025-03-07 08:49:02 +0530

    learned basics of scope

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,19 @@
+// var c = 30;
+
+// console.log(c);
+
+// {
+//     var a = 10;
+ 
+// }
+ 
+// console.log(a);
+function blockCheck() {
+  var a = 10;
+  console.log(a);
+}
+
+blockCheck();
+
+// console.log(b);
+
