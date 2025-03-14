75ba8316071bd4027b2377b1c85af92eee46bc39
Tejas Musale <tejasmusale99@gmail.com>
2025-03-14 21:27:13 +0530

    revised nullish coalescing and ternary

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -30,3 +30,26 @@ if(Object.keys(emptyObject).length === 0){
 // 1 == true  // output true
 // 1 === true   // output false
 
+/************Nullish Coalescing Operator (??): null undefined ***********/
+// (check krta hai agar value null or undefined hai first case me to o second value consider krta hai)
+
+let val1;
+val1 = 5 ?? 10
+console.log(val1)
+
+let val2;
+val2 = null ?? 15
+console.log(val2)
+
+let val3;
+val3 = undefined ?? 20
+console.log(val3)
+
+
+/**************Ternary operator ************/
+
+// syntax = condition ? true : false
+
+let name = "shubham";
+
+(name == "tejas") ? console.log("condition true") : console.log("condition false")
