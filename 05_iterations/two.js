608f21b05c947a12ff7fa403ff39f6f836e8fdac
Tejas Musale <tejasmusale99@gmail.com>
2025-03-15 20:58:54 +0530

    revised while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+/******* While Loop **********/
+
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i = i + 1;
+}
+
+/*********** Do WHile Loop ***********/
+
+let j = 1;
+do {
+   console.log(j)
+   j++
+} while (j<=10);
