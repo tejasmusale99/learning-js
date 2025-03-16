f16e9474b66097d0336747aea8d60a7679f42771
Tejas Musale <tejasmusale99@gmail.com>
2025-03-16 15:19:47 +0530

    revise for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,14 @@
+/******* in below case we get all the keys **********/
+
+for(const key in myObjecct){
+    // console.log(key)
+}
+
+/************** in below case we get key value pair ***************/
+
+for (const key in myObjecct){
+    console.log(key, ": ", myObjecct[key])
+}
+
+console.log(myObjecct.js)
+console.log(myObjecct["js"])
