e70af11121a26f30f7e132c08808714cffde9fd8
Tejas Musale <tejasmusale99@gmail.com>
2025-06-04 23:25:49 +0530

    revised js promise

---
+++ 10_advance_one/promises.js
@@ -0,0 +1,42 @@
+// const newPromise = new Promise(function(resolve,rej){
+//     setTimeout(function(){
+//         console.log("async task")
+//         resolve()
+//     },1000)   
+// });
+
+
+// newPromise.then(function(){
+//     console.log("promise consumed");
+// })
+
+// const proinseTwo = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve("tejas")
+//     },1000)
+// }).then(function(name){
+// console.log(name)
+// })
+
+const promiseThree = new Promise(function(resolve,reject){
+    let error = false;
+    if(!error){
+        setTimeout(function(){
+            resolve("tejas")
+        },1000)
+    }else{
+        reject('something went wrong')
+    }
+})
+
+async function consumeThreePromise(){ 
+try{
+    const response = await promiseThree
+    console.log(response);
+}catch(err){
+console.log(err);
+}
+}
+
+consumeThreePromise()
+
